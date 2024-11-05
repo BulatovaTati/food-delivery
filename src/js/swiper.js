f4625ad8b9d56699697efe1c138aac3fa29b491d
BulatovaTati <kohodatat@gmail.com>
2024-11-05 18:18:26 +0200

    Revert "try knew slider library"
    This reverts commit 13e9093e1823d8cd31e32d5e040da51eb3cb21f4.

--- src/js/swiper.js
+++
@@ -1,9 +0,0 @@
-import Splide from '@splidejs/splide';
-
-import '@splidejs/splide/css';
-import '@splidejs/splide/css/core';
-let elms = document.getElementsByClassName('splide');
-
-for (let i = 0; i < elms.length; i++) {
-  new Splide(elms[i]).mount();
-}
